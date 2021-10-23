46a44d36d27e0dc01621cbe1875985ec850da4af
Harshvardhan Basava <harshvardhanbsv@gmail.com>
2021-10-23 23:04:19 +0530

    ver1.0.0

--- MarvelSwift/Helpers/Credentials.swift
+++ MarvelSwift/Helpers/Credentials.swift
@@ -10,7 +10,10 @@ import SwiftUI
 //  Add your own public and private keys here to make API calls.
 //  You can generate the hash required for the API call either here
 //  in Swift code, or use any online generators.
-//  The usual pattern for generating a hash is (timestamp+privateKey+publicKey).
+
+//  The MD5 hash generator I wrote is in HomeViewModel.swift under ViewModel.
+
+//  The usual pattern for generating a Marvel API hash is (timestamp+privateKey+publicKey).
 //  For this project, I have generated timestamp using the Date().timeIntervalSince1970
 //  and converted it to a string to use it in the API call.
 
